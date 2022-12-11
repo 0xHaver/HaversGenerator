a996a22739592716b33ea0c4c6d6b1f26b1e33c8
0xHaver <119285803+0xHaver@users.noreply.github.com>
2022-12-10 19:38:45 -0600

    Create HaversGenerator.py

---
+++ HaversGenerator.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+# Create a list of all possible characters
+characters = list(string.ascii_uppercase + string.digits)
+
+# Ask the user how many codes they want to generate
+num_codes = int(input("How many codes would you like to generate? "))
+
+# Generate the specified number of unique codes
+codes = []
+while len(codes) < num_codes:
+    # Create a new code of 16 random characters
+    new_code = "".join(random.choices(characters, k=16))
+
+    # Add hyphens every 4 characters except at the end
+    new_code = "-".join(new_code[i:i+4] for i in range(0, len(new_code), 4))
+
+    # Add the code to the list if it's not already in the list
+    if new_code not in codes:
+        codes.append(new_code)
+
+# Print the list of codes
+for c in codes:
+    print(c)
